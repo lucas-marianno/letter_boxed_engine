cabb0ad0ed5704003f6027f091bd0a874728ad25
Lucas Marianno <lucas.marianno94@gmail.com>
2024-11-04 23:23:20 -0300

    bugfix: assets not loading when package is imported into a different project

--- lib/src/data/load_dictionary.dart
+++ lib/src/data/load_dictionary.dart
@@ -1,12 +1,20 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:letter_boxed_engine/src/domain/entities/game_language.dart';
 
 /// Loads and returns a dictionary containing only unique unique words sorted
 /// in order of most unique characters.
 Future<List<String>> loadDictionary(GameLanguage language) async {
-  final file = File('assets/${language.name}_valid_words_only.json');
+  final uri = Uri.parse(
+      'package:letter_boxed_engine/assets/${language.name}_valid_words_only.json');
+
+  final resolvedUri = await Isolate.resolvePackageUri(uri);
+  if (resolvedUri == null) {
+    throw Exception('Could not resolve URI for asset: $uri');
+  }
+  final file = File(resolvedUri.toFilePath());
   final data = jsonDecode(await file.readAsString()) as Map<String, dynamic>;
   final dictionary = data.keys.toSet();
 
